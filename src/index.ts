d91958a288cd95d2eb918f04a722ccdea3af1d06
Lim-Watt <wht462@outlook.com>
2025-03-07 22:05:36 +0800

    DEBUG

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+console.log(JSON.parse(process.argv[2]));
+console.log(JSON.parse(process.argv[3]));
+
 axios.post('https://www.luogu.com.cn/api/chat/new', JSON.parse(process.argv[2]), {
     headers: JSON.parse(process.argv[3])
 }).then(r => {
